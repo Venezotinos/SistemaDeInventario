f6817e89ff70d2e7ba15cf32c15d1a1803b0cdb8
Leonardo <leonardosolorzano16@gmail.com>
2019-10-12 19:34:23 -0300

    Clean solution, downgrade project version from 3.0 to 2.1, create Product controller and views, include product index in layoud.

---
+++ SistemaDeInventario/SistemaDeInventario/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeInventario.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public double BuyPrice { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public DateTime BuyDate { get; set; }
+
+        public DateTime SellDate { get; set; }
+
+        public ICollection<ProductCategory> Categories { get; set; }
+    }
+}
